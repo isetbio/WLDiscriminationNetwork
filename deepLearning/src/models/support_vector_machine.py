8613c36a27901d205caf62d0dbd53499fcd29659
FabianRei <f.abi.an@gmx.de>
2019-01-29 15:07:36 -0800

    generalize svm

--- deepLearning/src/models/support_vector_machine.py
+++ deepLearning/src/models/support_vector_machine.py
@@ -24,9 +24,9 @@ def write_svm_csv(acc, dprime, metric, out_path, lock=None, metric_name='contras
         lock.release()
 
 
-def get_svm_accuracy(path_mat, num_samples=15000):
+def get_svm_accuracy(path_mat, num_samples=15000, **kwargs):
     start = time.time()
-    meanData, meanDataLabels, dataContrast = get_h5mean_data(path_mat, includeContrast=True)
+    meanData, meanDataLabels, dataMetric = get_h5mean_data(path_mat, **kwargs)
     testDataFull, testLabelsFull = poisson_noise_loader(meanData, size=num_samples, numpyData=True)
     testDataFull = testDataFull.reshape(testDataFull.shape[0], -1)
     svc = svm.LinearSVC()
@@ -39,7 +39,7 @@ def get_svm_accuracy(path_mat, num_samples=15000):
     acc = np.mean(preds == y_test)
     dprime = calculate_dprime(np.stack([preds, y_test], axis=1))
     print(f'Accuracy is {acc}, Dprime is {dprime} num train samples is {num_train}, took {str(datetime.timedelta(seconds=time.time()-start))}.')
-    return acc, dprime, float(dataContrast[0])
+    return acc, dprime, float(dataMetric[0])
 
 
 if __name__ == '__main__':
@@ -47,4 +47,4 @@ if __name__ == '__main__':
     path_mat = '/share/wandell/data/reith/2_class_MTF_freq_experiment/frequency_1/5_samplesPerClass_freq_1_contrast_oo_0_000414616956.h5'
     sample_numbers = np.logspace(np.log10(500), np.log10(50000), num=15).astype(np.int)
     for num in sample_numbers:
-        get_svm_accuracy(path_mat, num_samples=num)
+        get_svm_accuracy(path_mat, num_samples=num, includeShift=True)

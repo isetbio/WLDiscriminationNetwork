155dfa7ec57b98bc78a5137c74326900ba5a3df4
FabianRei <f.abi.an@gmx.de>
2019-03-09 18:53:02 -0800

    create temporal data.

--- isetbio/s_createDataH5.m
+++ isetbio/s_createDataH5.m
@@ -6,7 +6,7 @@ for i = 1:length(contrastValues)
     start = tic;
     contrast = contrastValues(i);
     createResnetTrainingData('outPath', outPath, 'contrast', contrast, 'frequency', 1, 'signalGridSize', 1, 'signalLocation', 1, ...
-        'sceneSizeDegs', 2, 'gridZoom', 1, 'nTrialsNum', 1, 'nPathSteps', 10);
+        'sceneSizeDegs', 2, 'gridZoom', 1, 'nTrialsNum', 1000, 'nPathSteps', 10);
     sprintf("Time for for data generation with contrast of %d is %d seconds", contrast, round(toc(start)));
 end
  sprintf("Time for for whole program is %d seconds",round(toc(allStar)));

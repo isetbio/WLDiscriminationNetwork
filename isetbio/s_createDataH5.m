14a194e2ede858263d20f722beacb7947b38b320
FabianRei <f.abi.an@gmx.de>
2019-03-11 16:01:04 -0700

    newly created data :)

--- isetbio/s_createDataH5.m
+++ isetbio/s_createDataH5.m
@@ -1,12 +1,30 @@
-contrastValues = logspace(-3.5, log(0.5)/log(10), 18);
-outPath = '/share/wandell/data/reith/coneMosaik/temporal_case_freq1_var_contrasts/';
+contrastValues = logspace(-3, log(0.5)/log(10), 12);
+outPath = '/share/wandell/data/reith/coneMosaik/static_case_freq1_var_contrasts/';
 mkdir(outPath);
 allStar = tic;
 for i = 1:length(contrastValues)
     start = tic;
     contrast = contrastValues(i);
     createResnetTrainingData('outPath', outPath, 'contrast', contrast, 'frequency', 1, 'signalGridSize', 1, 'signalLocation', 1, ...
-        'sceneSizeDegs', 2, 'gridZoom', 1, 'nTrialsNum', 1000, 'nPathSteps', 10);
-    sprintf("Time for for data generation with contrast of %d is %d seconds", contrast, round(toc(start)));
+        'sceneSizeDegs', 2, 'gridZoom', 1, 'nTrialsNum', 1, 'nPathSteps', 1);
+    fprintf("Time for for data generation with contrast of %d is %d seconds", contrast, round(toc(start)));
 end
- sprintf("Time for for whole program is %d seconds",round(toc(allStar)));
+fprintf("Time for for whole program is %d seconds",round(toc(allStar)));
+ 
+ 
+ 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% past runs:
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% contrastValues = logspace(-3.5, log(0.5)/log(10), 18);
+% outPath = '/share/wandell/data/reith/coneMosaik/temporal_case_freq1_var_contrasts/';
+% mkdir(outPath);
+% allStar = tic;
+% for i = 1:length(contrastValues)
+%     start = tic;
+%     contrast = contrastValues(i);
+%     createResnetTrainingData('outPath', outPath, 'contrast', contrast, 'frequency', 1, 'signalGridSize', 1, 'signalLocation', 1, ...
+%         'sceneSizeDegs', 2, 'gridZoom', 1, 'nTrialsNum', 1000, 'nPathSteps', 10);
+%     sprintf("Time for for data generation with contrast of %d is %d seconds", contrast, round(toc(start)));
+% end
+%  sprintf("Time for for whole program is %d seconds",round(toc(allSt

3dc41899dd01f90b3be062f9b8dee880469aad6d
FabianRei <f.abi.an@gmx.de>
2019-03-12 12:01:36 -0700

    new job

--- deepLearning/visualization_scripts/cone_harmonic_calibration2.py
+++ deepLearning/visualization_scripts/cone_harmonic_calibration2.py
@@ -20,7 +20,7 @@ def get_csv_column(csv_path, col_name, sort_by=None, exclude_from=None):
 
 include_svm = True
 
-folder_paths = ['/share/wandell/data/reith/coneMosaik/static_case_freq1_var_contrasts_no_gabor/']
+folder_paths = ['/share/wandell/data/reith/coneMosaik/static_case_freq1_var_contrasts_no_gabor_90degs_rotated/']
 
 fig = plt.figure()
 # plt.grid(which='both')

3d402327d539bae315de0b822aa0b176df924f4e
FabianRei <f.abi.an@gmx.de>
2019-03-13 13:52:48 -0700

    1 decimal job

--- deepLearning/visualization_scripts/cone_harmonic_calibration2.py
+++ deepLearning/visualization_scripts/cone_harmonic_calibration2.py
@@ -20,7 +20,7 @@ def get_csv_column(csv_path, col_name, sort_by=None, exclude_from=None):
 
 include_svm = True
 
-folder_paths = ['/share/wandell/data/reith/coneMosaik/sensor_sanity_real_mean/', '/share/wandell/data/reith/coneMosaik/sensor_sanity_real_mean_eval_test/']
+folder_paths = ['/share/wandell/data/reith/coneMosaik/sanity_sensor_data/', '/share/wandell/data/reith/coneMosaik/sensor_sanity_point5_rounded/']
 
 fig = plt.figure()
 # plt.grid(which='both')
@@ -28,26 +28,26 @@ plt.xscale('log')
 plt.xlabel('contrast')
 plt.ylabel('dprime')
 plt.title(f"Contrast calibration for cone mosaic")
-plt.title(f"Sensor data - eval vs no eval test comparison")
+plt.title(f"Sensor data - rounded to rounded point 5 mean comparison")
 for i, p in enumerate(folder_paths):
     if i == 0:
         appendix = ' rounded mean'
     elif i == 1:
-        appendix = ' rounded mean eval test'
+        appendix = ' rounded to .5 mean'
     csv1 = os.path.join(p, 'results.csv')
     csv_svm = os.path.join(p, 'svm_results.csv')
     fname = 'cone_mosaic_contrasts_calibration_exclude_lower_vals_updated'
-    fname = 'sensor_data_eval no eval test'
+    fname = 'sensor_data_point5_mean_to_rounded_mean'
 
-    oo = get_csv_column(csv1, 'optimal_observer_d_index', sort_by='contrast')
-    nn = get_csv_column(csv1, 'nn_dprime', sort_by='contrast')
-    contrasts = get_csv_column(csv1, 'contrast', sort_by='contrast')
+    oo = get_csv_column(csv1, 'optimal_observer_d_index', sort_by='contrast', exclude_from=10**-6)
+    nn = get_csv_column(csv1, 'nn_dprime', sort_by='contrast', exclude_from=10**-6)
+    contrasts = get_csv_column(csv1, 'contrast', sort_by='contrast', exclude_from=10**-6)
 
     plt.plot(contrasts, oo, label='Ideal Observer'+appendix)
     plt.plot(contrasts, nn, label='ResNet18'+appendix)
     epsilon = 0.001
     if include_svm:
-        svm = get_csv_column(csv_svm, 'dprime_accuracy', sort_by='contrast')
+        svm = get_csv_column(csv_svm, 'dprime_accuracy', sort_by='contrast', exclude_from=10**-6)
         svm[svm >= (svm.max()-epsilon)] = oo.max()
         plt.plot(contrasts, svm, label='Support Vector Machine'+appendix)
 

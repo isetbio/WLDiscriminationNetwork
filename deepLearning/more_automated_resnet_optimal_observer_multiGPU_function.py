faed778ea410a926cc10750f5fc8c3b66166923a
Fabianrei <gFriessm20*>
2019-06-28 17:24:53 +0200

    create mtf files

--- deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
+++ deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
@@ -66,7 +66,7 @@ def run_on_folder(dirname, deeper_pls=False, NetClass=None, NetClass_param=None,
 if __name__ == '__main__':
     full_start = time.time()
     super_path = '/share/wandell/data/reith/redo_experiments/mtf'
-    # super_path = r'C:\Users\Fabian\Documents\data\windows2rsync\windows_data\mtf'
+    super_path = r'C:\Users\Fabian\Documents\data\windows2rsync\windows_data\mtf'
     fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
     fpaths.sort(key=lambda x: int(x.split('_')[-1]))
     for fpath in fpaths:

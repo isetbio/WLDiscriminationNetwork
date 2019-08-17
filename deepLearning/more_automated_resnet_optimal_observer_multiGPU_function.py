d4f8d8083805326c251c5ed32f7e083e69755e89
Fabianrei <gFriessm20*>
2019-08-17 11:42:38 +0200

    update matlab template creation

--- deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
+++ deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
@@ -62,14 +62,15 @@ def run_on_folder(dirname, deeper_pls=False, NetClass=None, NetClass_param=None,
     time.sleep(60)
     print("done!")
 
+
 if __name__ == '__main__':
     # disk mtf calculation. size is in pixel
     full_start = time.time()
-    super_path = '/share/wandell/data/reith/redo_experiments/disks_mtf_experiment/disk_more_contrast'
+    super_path = '/share/wandell/data/reith/redo_experiments/lines_mtf_experiments/mtf_lines_shift_higher_scene_res'
     fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
     fpaths.sort(key=lambda x: int(x.split('_')[-1]))
     for fpath in fpaths:
-        run_on_folder(fpath)
+        run_on_folder(fpath, shift=True)
     print(f"Whole program finished! It took {str(datetime.timedelta(seconds=time.time()-full_start))} hours:min:seconds")
 
 r"""
@@ -104,6 +105,16 @@ r"""
 #####################################################################
 Older runs for documentation purposes..
 #####################################################################
+if __name__ == '__main__':
+    # disk mtf calculation. size is in pixel
+    full_start = time.time()
+    super_path = '/share/wandell/data/reith/redo_experiments/disks_mtf_experiment/disk_more_contrast'
+    fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
+    fpaths.sort(key=lambda x: int(x.split('_')[-1]))
+    for fpath in fpaths:
+        run_on_folder(fpath)
+    print(f"Whole program finished! It took {str(datetime.timedelta(seconds=time.time()-full_start))} hours:min:seconds")
+######################################################################
 
 if __name__ == '__main__':
     # adjust shift/angle values for lines experiment

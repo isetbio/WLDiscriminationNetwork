8bd0e57267674bfe374cac89e172c177ea6c50da
Fabianrei <gFriessm20*>
2019-08-26 18:44:39 +0200

    hopefully works

--- deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
+++ deepLearning/more_automated_resnet_optimal_observer_multiGPU_function.py
@@ -66,11 +66,11 @@ def run_on_folder(dirname, deeper_pls=False, NetClass=None, NetClass_param=None,
 if __name__ == '__main__':
     # automata
     full_start = time.time()
-    super_path = '/share/wandell/data/reith/redo_experiments/redo_automaton/matlab_contrasts'
-    fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
-    # fpaths.sort(key=lambda x: int(x.split('_')[-1]))
-    for fpath in fpaths:
-        run_on_folder(fpath)
+    # super_path = '/share/wandell/data/reith/redo_experiments/redo_automaton/matlab_contrasts'
+    # fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
+    # # fpaths.sort(key=lambda x: int(x.split('_')[-1]))
+    # for fpath in fpaths:
+    #     run_on_folder(fpath)
     super_path = '/share/wandell/data/reith/redo_experiments/redo_automaton/plain_automata'
     fpaths = [p.path for p in os.scandir(super_path) if p.is_dir()]
     # fpaths.sort(key=lambda x: int(x.split('_')[-1]))
